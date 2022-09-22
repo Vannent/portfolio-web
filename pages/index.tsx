0fc0929c973f59ff864b3b87ecc7567895d5a090
Vannent <98601749+Vannent@users.noreply.github.com>
2022-09-22 14:22:18 -0700

    feat: add screens to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,22 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { AboutCP, HomeCP, ProjectsCP, ResourcesCP } from "../components";
 
 const Home: NextPage = () => {
   return (
-    <div className="{container}">
+    <div id="container">
       <Head>
         <title>Roberto Cedeno</title>
         <meta name="description" content="Software Engineer Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="{main}"></main>
-
-      <footer className=""></footer>
+      <main id="content" className="">
+        <HomeCP />
+        <AboutCP />
+        <ProjectsCP />
+        <ResourcesCP />
+      </main>
     </div>
   );
 };
