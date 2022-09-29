8bd32e0ccb8203c51790997524f60bb3580c6c34
Vannent <98601749+Vannent@users.noreply.github.com>
2022-09-29 12:56:38 -0700

    style: fix sizes

--- components/layout/Home.tsx
+++ components/layout/Home.tsx
@@ -4,20 +4,23 @@ import Personal from "../../assets/per.png";
 
 const Home = () => {
   return (
-    <div id="home" className="flex items-center justify-center">
-      <div className="h-full w-full mx-3 md:mx-10 lg:mx-32 xl:mx-72 flex items-center justify-center flex-col md:flex-row">
-        <div className="md:w-2/6 xl:w-2/4 w-full px-2 md:px-0 h-4/6 min-w-[300px] min-h-[300px] text-[#f6dcfd] flex  justify-center flex-col">
+    <div
+      id="home"
+      className="flex items-center justify-center flex-shrink-0 overflow-auto"
+    >
+      <div className="h-full w-full mx-3 md:mx-10 lg:mx-32 xl:mx-72 flex items-center justify-center flex-col md:flex-row ">
+        <div className="md:w-2/6 xl:w-2/4 w-full px-2 md:px-0 h-4/6 min-w-[300px] min-h-[300px] text-[#f6dcfd] flex  justify-center flex-col max-h-[500px]">
           <span className="h-[5px] xl:h-[10px] w-[40px] bg-[#a049b6]"></span>
-          <h4 className="font-extrabold font-lato uppercase tracking-wide text-[20px] xl:leading-10 xl:text-2xl">
+          <h4 className="font-extrabold font-lato uppercase tracking-wide text-[20px] xl:leading-10 xl:text-xl">
             Hello, World! I{"'"}m
           </h4>
-          <h1 className="font-bold font-oswald uppercase text-[55px] md:leading-none xl:text-7xl">
+          <h1 className="font-bold font-oswald uppercase text-[55px] md:leading-none xl:text-6xl">
             Roberto Cedeno
           </h1>
-          <h3 className="font-extrabold font-lato uppercase tracking-wider text-[35px] sm:leading-none md:leading-10 xl:leading-10 xl:text-5xl xl:tracking-wide">
+          <h3 className="font-extrabold font-lato uppercase tracking-wider text-[35px] sm:leading-none md:leading-10 xl:leading-10 xl:text-4xl xl:tracking-wide lg:text-base">
             Software Engineer
           </h3>
-          <p className="font-lato text-xl xl:text-3xl">
+          <p className="font-lato text-xl xl:text-2xl">
             {" "}
             I build and design applications for the web. I{"'"}m based in{" "}
             <b className="text-[#a049b6] font-extrabold">San Francisco, CA.</b>{" "}
@@ -28,7 +31,7 @@ const Home = () => {
             Learn More
           </button>
         </div>
-        <div className="lg:w-4/6 md:w-3/6 xl:w-2/4 min-w-[300px] h-4/6 md:flex items-center justify-center hidden">
+        <div className="lg:w-4/6 md:w-3/6 xl:w-2/4 min-w-[300px] md:max-w-[400px] lg:max-w-[500px] h-4/6 md:flex items-center justify-center hidden">
           <Image src={Personal} alt="Personal Image" />
         </div>
       </div>
